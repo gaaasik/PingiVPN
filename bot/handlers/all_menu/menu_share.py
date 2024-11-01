1c1e0e1565fa550d38fee04589e078249fda1c7c
Daniil.Gasanov <gasanov1top@gmail.com>
2024-11-01 17:50:58 +0300

    эмодзи к кнопкам

--- bot/handlers/all_menu/menu_share.py
+++ bot/handlers/all_menu/menu_share.py
@@ -7,19 +7,21 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.types import Message, CallbackQuery
 
 router = Router()
-name_bot=os.getenv('name_bot')
+name_bot = os.getenv('name_bot')
+
 
 # Текст сообщения для реферальной программы
 def generate_referral_text(invite_count: int):
     return (
         "🎉 *Приглашайте друзей и получайте бонусные дни!*\n\n"
         "1. *Нажмите Поделится* \n"
-        "2. *Перешлите сообщение другу* или *поделитесь вашей ссылкой*\n\n"
-        "- Друг получит *7 дней* бесплатного доступа!\n"
-        "- Вы получите *3 бонусных дня* за каждого друга, подключившегося *по ссылке*\n"
+        "2. *Перешлите сообщение другу* или *поделитесь ссылкой*\n\n"
+        "- Друг получит *7 дней* бесплатного доступа\n"
+        "- Вы получите *3 бонусных дня* за каждого друга, подключившегося *по вашей ссылке*\n"
         "- Если друг оформит подписку, вам начисляется *14 бонусных дней*\n\n"
         f"Количество приглашенных: {invite_count}"
     )
+
 
 # Клавиатура с кнопкой для реферальной ссылки
 # Клавиатура с кнопками "Показать реферальную ссылку" и "Главное меню"
@@ -30,6 +32,7 @@ def referral_menu_keyboard():
             [InlineKeyboardButton(text="🏠 Главное меню", callback_data="main_menu")]
         ]
     )
+
 
 # Универсальный обработчик для команды "/share" и кнопки "Пригласить"
 @router.message(Command("share"))
@@ -47,23 +50,24 @@ async def handle_share(event: Message | CallbackQuery, bot: Bot):
     if isinstance(event, Message):
         await event.answer(share_message, reply_markup=share_keyboard, parse_mode="Markdown")
     elif isinstance(event, CallbackQuery):
-        await event.message.edit_text(share_message, reply_markup=share_keyboard, parse_mode="Markdown",disable_web_page_preview=True)
+        await event.message.edit_text(share_message, reply_markup=share_keyboard, parse_mode="Markdown",
+                                      disable_web_page_preview=True)
         await event.answer()
+
 
 # Обработчик для кнопки "Показать реферальную ссылку"
 @router.callback_query(F.data == "show_referral_link")
 async def show_referral_link(callback_query: CallbackQuery, bot: Bot):
     chat_id = callback_query.from_user.id
-
+    name_bot = "GASIK_VPN_BOT"
     # Формируем реферальную ссылку
     referral_link = f"https://t.me/{name_bot}?start={chat_id}"
 
     # Текст сообщения с реферальной ссылкой
     referral_link_text = (
         f"🚀 *Разблокируйте любимые сайты и приложения!*\n\n"
-        f"Ваш друг рекомендует наш VPN для доступа к контенту без ограничений.\n"
-        f"Попробуйте 7 дней бесплатно!\n\n"
-        f"👉 Ваша реферальная ссылка:\n"
+        f"Ваш друг *рекомендует* наш VPN для доступа к контенту без ограничений.\n"
+        f"👉 Ваша ссылка на бота:\n\n"
         f"🔗 [Перейти к боту]({referral_link})\n\n"
         f"Для копирования:\n"
         f"```{referral_link}```"  # Отформатированная ссылка для копирования
@@ -79,4 +83,3 @@ async def show_referral_link(callback_query: CallbackQuery, bot: Bot):
     # Отправляем сообщение с реферальной ссылкой и кнопкой
     await callback_query.message.answer(referral_link_text, reply_markup=referral_button, parse_mode="Markdown")
     await callback_query.answer()
-
