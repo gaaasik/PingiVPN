e7d6035bc5ceafcb91813481b1ebad140d2a285c
Eis_Bot <danchik_99@inbox.ru>
2024-12-16 13:59:33 +0300

    Настроил уведомления

--- main.py
+++ main.py
@@ -226,7 +226,7 @@ async def main():
     # Настройка расписания уведомлений
     notification_scheduler.add_to_schedule("12:00", "UnsubscribedNotification")
     notification_scheduler.add_to_schedule("13:00", "TrialEndingNotification")
-    notification_scheduler.add_to_schedule("13:30", "PaymentReminder")  # Добавили PaymentReminder
+    notification_scheduler.add_to_schedule("14:10", "PaymentReminder")  # Добавили PaymentReminder
     #пропущенный пользователь
 
     # us= await UserCl.load_user(763159433)
